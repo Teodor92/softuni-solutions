6094fde41fd2d261d7ceec4e57201e38b6988775
Teodor Kurtev <teodor.ivanov92@gmail.com>
2022-02-05 22:10:12 +0200

    feat: refactored consultation code and added new projects

--- softuni-fundamentals-2022-january/solutions/04.Methods/01.SignOfIntegerNumbers/Program.cs
+++ softuni-fundamentals-2022-january/solutions/04.Methods/01.SignOfIntegerNumbers/Program.cs
@@ -6,24 +6,72 @@ namespace _01.SignOfIntegerNumbers
     {
         static void Main(string[] args)
         {
-            int number = int.Parse(Console.ReadLine());
-            PrintNumberSign(number);
+            //string greetingText = "Hello world";
+            //char[] chars = greetingText.ToCharArray();
+
+            ////for (int i = 0; i < greetingText.Length; i++)
+            ////{
+            ////    char currentChar = greetingText[i];
+            ////    Console.WriteLine(currentChar);
+            ////}
+
+            //foreach (char currentChar in greetingText)
+            //{
+            //    Console.WriteLine(currentChar);
+            //}
+
+            // System.LINQ
+
+            int grade = 33;
+            Console.WriteLine(grade);
+            grade = Decrement(grade);
+            Console.WriteLine(grade);
+
+            // Reference 333
+            int[] array = { 33 };
+
+            bool isInArray = IsInArray(array, 3);
+            Console.WriteLine(IsInArray(array, 3));
+
+            //Console.WriteLine(string.Join(",", array));
+            //DecrementFirstElement(array);
+            //Console.WriteLine(string.Join(",", array));
         }
 
-        static void PrintNumberSign(int number)
+        static int Decrement(int grade)
         {
-            if (number == 0)
+            grade--;
+            Console.WriteLine(grade);
+
+            return grade;
+        }
+
+        static bool IsInArray(int[] array, int index)
+        {
+            if (index >= 0 && index < array.Length)
             {
-                Console.WriteLine($"The number {number} is zero.");
-            }
-            else if (number > 0)
-            {
-                Console.WriteLine($"The number {number} is positive.");
+                return true;
             }
             else
             {
-                Console.WriteLine($"The number {number} is negative.");
+                return false;
             }
+        }
+
+
+        // Reference 333
+        static void DecrementFirstElement(int[] grade)
+        {
+            if (grade.Length == 0)
+            {
+                return;
+            }
+
+            //// Reference 444
+            //grade = new int[44];
+
+            grade[0]--;
+            Console.WriteLine(grade);
         }
     }
 }
