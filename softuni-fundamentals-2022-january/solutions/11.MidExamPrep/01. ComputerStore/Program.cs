6bdb5a8d8f1f0f07ecde8a3e83536309257685d0
Teodor Kurtev <teodor.ivanov92@gmail.com>
2022-02-12 21:04:46 +0200

    feat: added blank projects

---
+++ softuni-fundamentals-2022-january/solutions/11.MidExamPrep/01. ComputerStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._ComputerStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
