452393d3924df9aa508b5f8202410b687cf94af8
Teodor Kurtev <teodor.ivanov92@gmail.com>
2023-09-12 09:35:42 +0300

    feat: added demo code

--- softuni-fundamentals-and-unit-testing-2023-september/solutions/01.FirstStepsInProgramming/00.Demos/Program.cs
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/01.FirstStepsInProgramming/00.Demos/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace _00.Demos
+namespace _00.Demos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // int numberOfApples = 3;
+            // double numberPi = 3.14;
+            // string helloSoftUniMessae = "Hello, SoftUni!";
+            // char symbolForTheLetterA = 'A';
+
+            // string message = Console.ReadLine();
+            // Console.WriteLine(message);
+
+            //double magicNumber = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+            //Console.WriteLine(magicNumber);
+
+            //Console.WriteLine("dddd  dddd 9149jasf 814 $%^&");
+
+            //char test = "A";
+            //char test1 = 'A';
+
+            //string input = Console.ReadLine();
+            //int number = int.Parse(input);
+            //Console.WriteLine(number);
+
+            //int a = 3;
+            //int b = 5;
+            //int number = a + b;
+            //Console.WriteLine(number);
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int number = a - b;
+            //Console.WriteLine(number);
+
+            //string number = Console.ReadLine();
+            //double parsedNumber = double.Parse(number);
+            //double sum = parsedNumber / parsedNumber;
+            //Console.WriteLine(number);
+
+            //int a = 2 / 2;
+            //int b = 4 / 3;
+            //double c = 4 / 3.0;
+            //Console.WriteLine();
+            /// 1.987
+            ///
+
+            //var a = 5;
+            //var b = 5;
+
+            //var result = name != "Pesho";
+            //Console.WriteLine(result);
+
+            string name = "Dragan";
+            if (name == "Ivan")
+            {
+                Console.WriteLine("I'm Ivan!");
+            }
+            else if (name == "Dragan")
+            {
+                Console.WriteLine("I'm Dragan");
+            }
+            else
+            {
+                Console.WriteLine("I'm not Ivan");
+            }
+
+            Console.WriteLine("Hello " + "World!");
+            Console.WriteLine("Hello " + 'A');
+            string message = "asdasd" + 23 + "Hello ";
         }
     }
 }
