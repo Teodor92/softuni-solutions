f9d2eab203d61fd6513fca8da73609da88fbedaf
Teodor Kurtev <teodor.ivanov92@gmail.com>
2023-10-02 13:15:59 +0300

    feat: added template for new forth lecture

---
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/04.ForLoops/05.NumbersFrom1ToNOver3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.NumbersFrom1ToNOver3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/04.ForLoops/06.ExamCountDown/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06.ExamCountDown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
