8cdce8e39be54670a8496e0ab0c9619f9f17d145
Teodor Kurtev <teodor.ivanov92@gmail.com>
2023-10-30 09:34:07 +0200

    feat: added tests

--- softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/03.CenterPoint/TestApp.Tests/CenterPointTests.cs
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/03.CenterPoint/TestApp.Tests/CenterPointTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace TestApp.Tests;
@@ -8,30 +8,90 @@ public class CenterPointTests
     [Test]
     public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        double firstPointX = 1;
+        double firstPointY = 1;
+        double secondPointX = 2;
+        double secondPointY = 2;
+
+        string expected = $"({firstPointX}, {firstPointY})";
+        // Act
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        double firstPointX = 2;
+        double firstPointY = 2;
+        double secondPointX = 1;
+        double secondPointY = 1;
+
+        string expected = $"({secondPointX}, {secondPointY})";
+        // Act
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        double firstPointX = 1;
+        double firstPointY = 1;
+        double secondPointX = 1;
+        double secondPointY = 1;
+
+        string expected = $"({firstPointX}, {firstPointY})";
+        // Act
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        double firstPointX = -1;
+        double firstPointY = -1;
+        double secondPointX = 1;
+        double secondPointY = 1;
+
+        string expected = $"({firstPointX}, {firstPointY})";
+        // Act
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
     {
-        // TODO: implement the test
+        // Arrange
+        double firstPointX = 2;
+        double firstPointY = 2;
+        double secondPointX = -2;
+        double secondPointY = -2;
+
+        string expected = $"({secondPointX}, {secondPointY})";
+        // Act
+
+        string result = CenterPoint.GetClosest(firstPointX, firstPointY, secondPointX, secondPointY);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 }
--- softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/04.FoldArray/TestApp.Tests/FoldSumTests.cs
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/04.FoldArray/TestApp.Tests/FoldSumTests.cs
@@ -1,17 +1,22 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace TestApp.Tests;
 
 public class FoldSumTests
 {
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new[] { 1, 1, 1, 1 }, "2 2")]
+    [TestCase(new int[] {  }, "")]
+    [TestCase(new[] { 1, 2, 3, 4 }, "3 7")]
+    [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, "7 7 7 19 19 19")]
     public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
     {
-        // TODO: implement the test and finish the test cases
+        // Arrange
+        // Act
+        string result = FoldSum.FoldArray(arr);
+
+        // Assert
+        Assert.AreEqual(expected, result);
     }
 }
