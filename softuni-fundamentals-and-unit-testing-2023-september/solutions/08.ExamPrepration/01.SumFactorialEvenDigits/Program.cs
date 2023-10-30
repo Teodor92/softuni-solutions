f46048591ad681d66d5de450ca987044a05d7975
Teodor Kurtev <teodor.ivanov92@gmail.com>
2023-10-30 09:12:25 +0200

    feat: added code task solutions

--- softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/01.SumFactorialEvenDigits/Program.cs
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/01.SumFactorialEvenDigits/Program.cs
@@ -1,1 +1,27 @@
-Console.WriteLine("Hello World!");
+int inputNumber = int.Parse(Console.ReadLine());
+int totalResult = 0;
+
+while (inputNumber != 0)
+{
+    int digit = inputNumber % 10;
+
+    if (digit % 2 == 0)
+    {
+        totalResult += CalucateFactorial(digit);
+    }
+
+    inputNumber = inputNumber / 10;
+}
+
+Console.WriteLine(totalResult);
+
+int CalucateFactorial(int number)
+{
+    int result = 1;
+    while (number != 1)
+    {
+        result = result * number;
+        number = number - 1;
+    }
+    return result;
+}
--- softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/02.MiddleElements/Program.cs
+++ softuni-fundamentals-and-unit-testing-2023-september/solutions/08.ExamPrepration/02.MiddleElements/Program.cs
@@ -1,1 +1,13 @@
-Console.WriteLine("Hello World");
+double[] inputArray = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+if (inputArray.Length == 0)
+{
+    Console.WriteLine(0);
+}
+
+int leftMiddleElementIndex = inputArray.Length / 2;
+double middleElementsSum = (inputArray[leftMiddleElementIndex] + inputArray[leftMiddleElementIndex - 1]) / 2;
+Console.WriteLine($"{middleElementsSum:F2}");
